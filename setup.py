ccbb48e98fc6b3e0cf4c8b9bef2b30819a7b49be
Rok Roskar <roskarr@ethz.ch>
2017-04-26 12:25:19 +0200

    reorganization for testing

--- setup.py
+++ setup.py
@@ -7,18 +7,18 @@ import os
 
 currdir = os.getcwd()
 
-setup(name="spark_fof",
+setup(name="spark_fof_test",
       author="Rok Roskar",
       author_email = "roskarr@ethz.ch",
-      package_dir = {'spark_fof/': ''},
-      packages = ['spark_fof', 'spark_fof/fof'],
-      ext_modules = cythonize([Extension("spark_fof.spark_fof_c", 
-                                         sources = ["spark_fof/spark_fof_c.pyx"], 
-                                         include_dirs=[numpy.get_include(), 'spark_fof/fof']),
-                               Extension("spark_fof.fof.fof", 
-                                         sources = ["spark_fof/fof/fof.pyx"],
+      package_dir = {'spark_fof_test/': ''},
+      packages = ['spark_fof_test', 'spark_fof_test/fof'],
+      ext_modules = cythonize([Extension("spark_fof_test.spark_fof_c", 
+                                         sources = ["spark_fof_test/spark_fof_c.pyx"], 
+                                         include_dirs=[numpy.get_include(), 'spark_fof_test/fof']),
+                               Extension("spark_fof_test.fof.fof", 
+                                         sources = ["spark_fof_test/fof/fof.pyx"],
                                          libraries = ['kd'],
-                                         extra_compile_args = ['-I', os.path.join(currdir,'spark_fof/fof')],
-                                         extra_link_args = ['-L',os.path.join(currdir,'spark_fof/fof')],
+                                         extra_compile_args = ['-I', os.path.join(currdir,'spark_fof_test/fof')],
+                                         extra_link_args = ['-L',os.path.join(currdir,'spark_fof_test/fof')],
                                          include_dirs = [numpy.get_include()])])
 )
